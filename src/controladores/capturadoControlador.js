39b726ce69da76aa99e053ca6ac0f193327f1566
Otacon01 <52942653+Otacon01@users.noreply.github.com>
2024-08-12 20:40:11 -0500

    Update capturadoControlador.js

--- src/controladores/capturadoControlador.js
+++ src/controladores/capturadoControlador.js
@@ -3,8 +3,8 @@ const { Capturado } = require('../baseDatos');
 
 const capturarPokemon = async (req, res) => {
   try {
-    const compra = await Capturado.create(req.body);
-    res.status(201).json({ message: 'Pokemon capturado' });
+    const capturado = await Capturado.create(req.body);
+    res.status(201).json({ message: 'Pokemon capturado',capturado });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
